59ad1dd884fb9b71be5677c813e8de0960f193ec
Joanthan Meza <jduplantier44@gmail.com>
2023-05-24 22:38:56 -0500

    show images

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 # from .views import *
 from . import views
 
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('nosotros/', views.nosotros, name='nosotros'),
     path('libros/', views.libros, name='libros'),
     path('crear_libro/', views.crear_libro, name='crear_libro'),
     path('editar_libro/', views.editar_libro, name='editar_libro'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
