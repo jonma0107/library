ad81af2d3b886f56b22690843f1d0fb30980398e
JonathanMeza0107 <mbjonathan@soy.sena.edu.co>
2023-05-28 17:38:50 -0500

    delete and edit

--- app/urls.py
+++ app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('nosotros/', views.nosotros, name='nosotros'),
     path('libros/', views.libros, name='libros'),
     path('crear_libro/', views.crear_libro, name='crear_libro'),
-    path('editar_libro/', views.editar_libro, name='editar_libro'),
+    path('editar_libro/<int:id>', views.editar_libro, name='editar_libro'),
+    path('eliminar_libro/<int:id>', views.eliminar_libro, name='eliminar_libro'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Libro
 from .forms import LibroForm
@@ -15,8 +15,22 @@ def libros(request):
     return render(request, 'libros/index.html', {'libros':libros})      
 
 def crear_libro(request):
-    formulario = LibroForm(request.POST or None )
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')    
     return render(request, 'libros/crear.html', {'formulario':formulario})
 
-def editar_libro(request):
-    return render(request, 'libros/editar.html')
+def editar_libro(request, id):
+    libro = Libro.objects.get(id=id)
+    formulario = LibroForm(request.POST or None, request.FILES or None, instance=libro)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/editar.html', {'formulario': formulario})    
+
+def eliminar_libro(request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('libros')    
+
